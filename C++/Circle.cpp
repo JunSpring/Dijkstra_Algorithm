aec73a2fcf40b96f3833ae4bc37c07740e3661e2
JunSpring <wjdwnsqja08@gmail.com>
2021-11-07 16:40:05 +0900

    P.125 ~ P.129

--- C++/Circle.cpp
+++ C++/Circle.cpp
@@ -1,41 +1,24 @@
-#include<iostream>
-#include<string>
+// 구조체 작성
+
+#include <iostream>
 using namespace std;
 
-class Circle
+struct StructCircle
 {
 private:
 	int radius;
-
 public:
+	StructCircle(int r) { radius = r; }
 	double getArea();
-
-	Circle();
-	Circle(int r) // 인라인함수의 예 1
-	{
-		radius = r;
-		cout << "반지름 " << radius << " 원 생성" << endl;
-	}
-	~Circle();
 };
 
-inline Circle::Circle() // 인라인함수의 예 2
-{
-	radius = 1;
-	cout << "반지름 " << radius << " 원 생성" << endl;
-}
-
-double Circle::getArea()
+double StructCircle::getArea()
 {
 	return 3.14 * radius * radius;
 }
 
-Circle::~Circle()
-{
-	cout << "반지름 " << radius << " 원 소멸" << endl;
-}
-
 int main()
 {
-
+	StructCircle waffle(3);
+	cout << "면적은 " << waffle.getArea();
 }
